d832c6a8134f530671b1ee9d60bc67eebb2c5241
Yuri Tabacof <yuritabacof@gmail.com>
2024-05-01 23:50:31 -0300

    css+logica do jogo

--- football-bingo/src/App.js
+++ football-bingo/src/App.js
@@ -8,10 +8,9 @@ function App() {
   const [attempts, setAttempts] = useState({});
   const [teamInfo, setTeamInfo] = useState({});
   const [selectedTeamIds, setSelectedTeamIds] = useState([]);
-  const [isDataFetched, setIsDataFetched] = useState(false);
   const [resultsShown, setResultsShown] = useState(false);
 
-  const teamIds = ['13', '418', '131', '281', '985', '631', '31', '11', '148', '506', '5', '46', '12', '113', '583', '244', '1041', '162', '16', '27'];
+  const allTeamIds = ['13', '418', '131', '281', '985', '631', '31', '11', '148', '506', '5', '46', '12', '113', '583', '244', '1041', '162', '16', '27'];
   const teamNames = {
     '13': 'At. Madrid', '418': 'Real Madrid', '131': 'Barcelona', '281': 'Manchester City',
     '985': 'Manchester United', '631': 'Chelsea', '31': 'Liverpool', '11': 'Arsenal',
@@ -19,7 +18,7 @@ function App() {
     '113': 'Napoli', '46': 'Inter', '583': 'PSG', '244': 'Marseille',
     '1041': 'Lyon', '162': 'Monaco', '16': 'Borussia Dortmund', '27': 'Bayern Munich'
   };
-  const seleIds = ['3439', '3377', '3299', '3437', '3300', '3816', '3449', '3376', '3556'];
+  const allSeleIds = ['3439', '3377', '3299', '3437', '3300', '3816', '3449', '3376', '3556'];
   const seleNames = {'3439': 'Brasil', '3377': 'França', '3299': 'Inglaterra', '3437': 'Argentina', '3300': 'Portugal', '3816': 'Colombia', '3449': 'Uruguai', '3376': 'Italia', '3556': 'Croacia'};
 
   // Definição da função sleep
@@ -27,72 +26,91 @@ function App() {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  useEffect(() => {
-    if (!isDataFetched) {
-      const shuffledTeams = teamIds.sort(() => 0.5 - Math.random()).slice(0, 16);
-      setSelectedTeamIds(shuffledTeams);
-
-      async function fetchTeamImages() {
-        const images = {};
-        for (const teamId of shuffledTeams) {
-          try {
-            await sleep(1000); // Usando a função sleep definida acima
-            const response = await axios({
-              method: 'GET',
-              url: 'https://transfermarket.p.rapidapi.com/clubs/get-header-info',
-              params: { id: teamId, domain: 'de' },
-              headers: {
-                'X-RapidAPI-Key': '15cfe02b82msh84be3d25168c166p145b20jsn87329f715d09',
-                'X-RapidAPI-Host': 'transfermarket.p.rapidapi.com'
-              }
-            });
-            if (response.data && response.data.club && response.data.club.image) {
-              images[teamId] = response.data.club.image;
-            }
-          } catch (error) {
-            console.error(`Error fetching team image for ${teamId}:`, error);
-          }
-        }
-        setTeamInfo(images);
-      }
-      
-  
-      async function fetchPlayers() {
-        const fetchedPlayers = [];
-        for (let i = 0; i < 10; i++) {
-          const randomTeamId = shuffledTeams[Math.floor(Math.random() * shuffledTeams.length)];
-          try {
-            const response = await axios({
-              method: 'GET',
-              url: 'https://transfermarket.p.rapidapi.com/clubs/get-squad',
-              params: { id: randomTeamId, domain: 'de' },
-              headers: {
-                'X-RapidAPI-Key': '15cfe02b82msh84be3d25168c166p145b20jsn87329f715d09',
-                'X-RapidAPI-Host': 'transfermarket.p.rapidapi.com'
-              }
-            });
-            if (response.status === 200 && response.data.squad.length > 0) {
-              const randomIndex = Math.floor(Math.random() * response.data.squad.length);
-              fetchedPlayers.push({
-                ...response.data.squad[randomIndex],
-                teamId: randomTeamId
-              });
-            }
-          } catch (error) {
-            console.error(`Error fetching players:`, error);
-          }
-        }
-        console.log("Lista de jogadores:", fetchedPlayers);
-        setPlayers(fetchedPlayers);
-        setIsDataFetched(true);
-      }
-    
-      fetchPlayers();
-      fetchTeamImages();
-
+  // Definição da função getRandomElements
+  function getRandomElements(arr, numElements) {
+    if (arr.length < numElements) {
+        return "A lista contém menos de " + numElements + " elementos.";
     }
-  }, [isDataFetched, teamIds]); 
+    const shuffled = [...arr].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, numElements);
+  }
 
+  // Embaralja os elementos da lista
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Troca elementos
+    }
+    return array;
+  }
+  
+
+  useEffect(() => {
+    const selectedTeamIds = getRandomElements(allTeamIds, 10);
+    const selectedSeleIds = getRandomElements(allSeleIds, 6);
+    setSelectedTeamIds(selectedTeamIds.concat(selectedSeleIds));
+  
+    async function fetchTeamImages() {
+      const images = {};
+      for (const Id of selectedTeamIds.concat(selectedSeleIds)) {
+        try {
+          await sleep(100); // Delay para evitar muitos pedidos simultâneos
+          const response = await axios({
+            method: 'GET',
+            url: 'https://transfermarket.p.rapidapi.com/clubs/get-header-info',
+            params: { id: Id, domain: 'de' },
+            headers: {
+              'X-RapidAPI-Key': 'dbdd1d09a4mshf3f47123a34ce0bp1fd8d3jsnb9806488c8d7',
+              'X-RapidAPI-Host': 'transfermarket.p.rapidapi.com'
+            }
+          });
+          if (response.data && response.data.club && response.data.club.image) {
+            images[Id] = response.data.club.image;
+          }
+        } catch (error) {
+          console.error(`Error fetching team image for ${Id}:`, error);
+        }
+      }
+      setTeamInfo(images);
+    }
+  
+    async function fetchPlayers() {
+      const fetchedPlayers = [];
+      // Busca 2 jogadores de cada time/seleção
+      for (const teamId of selectedTeamIds.concat(selectedSeleIds)) {
+        try {
+          await sleep(10); // Delay para evitar muitos pedidos simultâneos
+          const response = await axios({
+            method: 'GET',
+            url: 'https://transfermarket.p.rapidapi.com/clubs/get-squad',
+            params: { id: teamId, domain: 'de' },
+            headers: {
+              'X-RapidAPI-Key': 'dbdd1d09a4mshf3f47123a34ce0bp1fd8d3jsnb9806488c8d7',
+              'X-RapidAPI-Host': 'transfermarket.p.rapidapi.com'
+            }
+          });
+          if (response.status === 200 && response.data.squad.length > 1) {
+            const indexes = getRandomElements([...Array(response.data.squad.length).keys()], 2);
+            indexes.forEach(index => {
+              fetchedPlayers.push({
+                ...response.data.squad[index],
+                teamId: teamId
+              });
+            });
+          }
+        } catch (error) {
+          console.error(`Error fetching players from team ${teamId}:`, error);
+        }
+      }
+      console.log("Lista de jogadores:", fetchedPlayers);
+      setPlayers(shuffleArray(fetchedPlayers));
+    }
+  
+    fetchPlayers();
+    fetchTeamImages();
+  
+  }, []);
+  
   const handleAttempt = (teamId) => {
     const isCorrect = players[currentIndex].teamId === teamId;
     setAttempts(prev => ({
@@ -117,19 +135,31 @@ function App() {
   if (resultsShown) {
     const correctCount = Object.values(attempts).filter(val => val === true).length;
     return <div className="App">
-      <h1>ACABOU RESULTADO: {correctCount}/{players.length}</h1>
+      <h1>ACABOU RESULTADO: {correctCount}/16</h1>
     </div>;
   }
 
-  if (players.length === 0) return <div>Loading...</div>;
-
+  if (players.length === 0) {
+    return <div className="loading">Loading...</div>;
+  }
+  
   return (
     <div className="App">
       <h1>Football Bingo</h1>
       <div className="player-info">
         <img src={players[currentIndex]?.image} alt={players[currentIndex]?.name} />
         <h3>{players[currentIndex]?.name}</h3>
-        <button onClick={handleSkip}>Skip</button>
+        <button 
+          onClick={handleSkip} 
+          style={{
+            fontFamily: 'Bebas Neue, sans-serif', 
+            height: '30px', // Definindo a altura do botão
+            padding: '5px 10px', // Ajuste o padding para melhor encaixe do texto, se necessário
+            fontSize: '16px' // Ajuste o tamanho da fonte, se necessário
+          }}
+        >
+          Skip
+        </button>        
         <p>Jogadores restantes: {players.length - currentIndex - 1}</p>
       </div>
       <div className="team-buttons">
@@ -139,10 +169,18 @@ function App() {
             disabled={attempts[teamId] !== undefined}
             className={attempts[teamId] !== undefined ? (attempts[teamId] ? 'correct' : 'incorrect') : ''}
             onClick={() => handleAttempt(teamId)}
-          >
-            <img src={teamInfo[teamId] || ''} alt={teamNames[teamId]} style={{ width: '30px', height: '30px' }} />
-            {teamNames[teamId]}
-          </button>
+            style={{
+              backgroundImage: `url(${teamInfo[teamId] || ''})`,
+              backgroundSize: 'contain',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              padding: '20px',
+              height: '100px',
+              width: '100px',
+              border: '2px solid #fff',
+              borderRadius: '10px'
+            }}
+            />
         ))}
       </div>
     </div>
